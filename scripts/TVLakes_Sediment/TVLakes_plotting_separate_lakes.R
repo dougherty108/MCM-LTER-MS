ab4a9c8b76d2c5bdc2a189df17796663d3d5fe40
Charlie Doughertye <cedougherty@wisc.edu>
2025-03-06 14:36:59 -0600

    Updated this to have properly labeled axes and better names for columns.

--- scripts/TVLakes_Sediment/TVLakes_plotting_separate_lakes.R
+++ scripts/TVLakes_Sediment/TVLakes_plotting_separate_lakes.R
@@ -17,7 +17,7 @@ get_type <- function(filename) {
 }
 
 # Create output directories for each type
-output_base <- "~/Documents/R-Repositories/MCM-LTER-MS/plots/LANDSAT/20250301"
+output_base <- "~/Documents/R-Repositories/MCM-LTER-MS/plots/LANDSAT/20250306"
 dir.create(output_base, showWarnings = FALSE)
 
 types <- unique(na.omit(sapply(files, get_type)))
@@ -32,10 +32,10 @@ for (i in 1:length(files)) {
   raster_file <- raster(files[[i]])
   raster_df <- as.data.frame(raster_file, xy = TRUE) |> 
     drop_na()
-  colnames(raster_df)[3] <- "value"
+  #colnames(raster_df)[3] <- ""
   
   raster_df = raster_df |> 
-    mutate(value = (1 - value))
+    mutate(sediment_abundance = (1 - ice_endmember))
   
   year <- str_extract(files[[i]], "20\\d{2}-\\d{2}-\\d{2}")
   type <- get_type(files[[i]])
@@ -44,13 +44,13 @@ for (i in 1:length(files)) {
     plot_path <- file.path(output_base, type, paste0("LANDSAT_plot_", type, "_", year, ".png"))
     
     ggplot() +
-      geom_raster(data = raster_df, aes(x = x, y = y, fill = value)) +
+      geom_raster(data = raster_df, aes(x = x, y = y, fill = sediment_abundance)) +
       coord_sf() +
       scale_fill_gradientn(colors = met_palette) +
-      labs(title = paste0(type, " - ", year), x = "Northing", y = "Easting") +
+      labs(title = paste0(type, " - ", year), x = "Easting", y = "Northing") +
       theme_minimal()
     
-    setwd("~/Documents/R-Repositories/MCM-LTER-MS/plots/LANDSAT/20250301")
+    setwd("~/Documents/R-Repositories/MCM-LTER-MS/plots/LANDSAT/20250306")
     ggsave(filename = plot_path)
     print(paste0("Saved plot for ", type, " - ", year, " (", i, "/", length(files), ")"))
   }
