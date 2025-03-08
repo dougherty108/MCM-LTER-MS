a02e262879ebb7d3f5f9fb31f7cd501ff8594afe
Charlie Doughertye <cedougherty@wisc.edu>
2025-03-08 15:13:41 -0600

    Updating this to have the sed to ice comparison

--- scripts/TVLakes_Sediment/manuscript_plots_chapter1_GEE_Albedo.R
+++ scripts/TVLakes_Sediment/manuscript_plots_chapter1_GEE_Albedo.R
@@ -79,6 +79,83 @@ ggsave("plots/manuscript/chapter 1/whole_lake_vs_BB_ice_abundance.png", dpi = 70
        height = 8, width = 12)
 
 
+## load lake ice: 
+lakeice1 <- read_csv("data/lake ice/mcmlter-lake-ice_thickness-20230726 (1).csv") |> 
+  mutate(date_time = mdy_hm(date_time), 
+         month = month(date_time), 
+         year = year(date_time),
+         year = as.numeric(year),
+         z_water_m = z_water_m*-1) |> 
+  filter(lake == "Lake Fryxell" | lake == "Lake Hoare" | lake == "East Lake Bonney" | lake == "West Lake Bonney") |> 
+  #filter(year >= 2016) |> 
+  filter(!grepl("^B", location_name))
+
+ggplot(lakeice1, aes(date_time, z_water_m)) + 
+  geom_point() + 
+  geom_smooth(se = T) + 
+  facet_wrap(vars(lake)) + 
+  theme_linedraw(base_size = 15) + 
+  xlab("Date") + ylab("Percent Coverage (%)") + 
+  ggtitle("Ice thickness (m) 1993-2024", 
+          subtitle = "ice to water measurement")
+
+ggsave("plots/manuscript/chapter 1/ice_thickness_total_years.png", dpi = 700, 
+       height = 8, width = 12)
+
+lakeice = lakeice1 |> 
+  filter(year >= 2016)
+
+li_summary = lakeice |> 
+  group_by(year, month, lake) |> 
+  summarize(mean_thickness = mean(z_water_m, na.rm = T))
+
+## sediment
+sed_monthly <- sedi |> 
+  group_by(year, month, lake, buffer_distance) |> 
+  summarize(mean_sed = mean(sediment_abundance, na.rm = T)) |> 
+  print()
+
+fulljoined = full_join(sed_monthly, li_summary) |> 
+  drop_na()
+
+##### plot
+ggplot(fulljoined, aes(mean_sed, mean_thickness, color = lake)) + 
+  geom_smooth(method = "lm", se = F) + 
+  geom_point() + 
+  facet_wrap(vars(buffer_distance), scales = "free") + 
+  ggtitle("October-February", 
+          subtitle = "whole lake average") + 
+  scale_color_brewer(palette = "Set1") +
+  theme_linedraw() 
+
+### now remove November and October Values
+fulljoin_filter <- fulljoined |> 
+  filter(month == 12 | month == 1)
+
+ggplot(fulljoin_filter, aes(mean_sed, mean_thickness, color = lake)) + 
+  geom_smooth(method = "lm", se = F) + 
+  geom_point() + 
+  facet_wrap(vars(buffer_distance), scales = "free") + 
+  ggtitle("December-January", 
+          subtitle = "whole lake average") + 
+  scale_color_brewer(palette = "Set1") +
+  theme_linedraw(base_size = 15) 
+
+# now filter for only early year estimates, October - December
+oct_dec_fulljoin <- fulljoined |> 
+  filter(month == 10 | month == 11 | month == 12)
+
+ggsave("plots/GEE/alllakes/analysis plots/wholelakesed_vsthickness.png", width = 6.5, height = 3.5, units = "in", dpi = 500)
+
+ggplot(oct_dec_fulljoin, aes(mean_sed, mean_thickness, color = lake)) + 
+  geom_smooth(method = "lm", se = F) + 
+  geom_point() + 
+  facet_wrap(vars(buffer_distance), scales = "free") + 
+  ggtitle("October - December", 
+          subtitle = "whole lake average") + 
+  scale_color_brewer(palette = "Set1") +
+  theme_linedraw(base_size = 15) 
+
 
 
 ######### investing different buffering distances #########
