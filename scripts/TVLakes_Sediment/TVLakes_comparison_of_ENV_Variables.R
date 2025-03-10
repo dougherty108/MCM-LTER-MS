d508dcfa87addd4f651296e95775e8c7876936c3
Charlie Doughertye <cedougherty@wisc.edu>
2025-03-10 15:26:54 -0500

    join em up.

--- scripts/TVLakes_Sediment/TVLakes_comparison_of_ENV_Variables.R
+++ scripts/TVLakes_Sediment/TVLakes_comparison_of_ENV_Variables.R
@@ -12,19 +12,22 @@ BOYM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_
   mutate(date_time = ymd_hms(date_time), 
          year = year(date_time),
          month = month(date_time),
-         week = week(date_time))
+         week = week(date_time)) |> 
+  select(c(metlocid, date_time, airtemp_3m_degc, wspd_ms, swradin_wm2))
 
 HOEM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_hoem_15min-20250205.csv") |> 
   mutate(date_time = ymd_hms(date_time), 
          year = year(date_time),
          month = month(date_time),
-         week = week(date_time))
+         week = week(date_time)) |> 
+  select(c(metlocid, date_time, airtemp_3m_degc, wspd_ms, swradin_wm2))
 
 FRLM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_frlm_15min-20250205.csv") |> 
   mutate(date_time = ymd_hms(date_time), 
          year = year(date_time),
          month = month(date_time),
-         week = week(date_time))
+         week = week(date_time)) |> 
+  select(c(metlocid, date_time, airtemp_3m_degc, wspd_ms, swradin_wm2))
 
 # add in data
 setwd("~/Documents/R-Repositories/MCM-LTER-MS")
@@ -38,4 +41,8 @@ ice_thickness <- read_csv("data/lake ice/mcmlter-lake-ice_thickness-20250218_0.c
 # goal here is to compare weather variables with ice thicknesses. Could do a 
 # weekly mean
 ice_thickness_weekly <- ice_thickness |> 
-  
+  rename(lake = location_name) |> 
+  group_by(year, week, lake) |> 
+  summarize(z_water_m = mean((z_water_m*-1), na.rm = T), 
+            z_ice_m = mean(z_ice_m, na.rm = T))
+
