2f2c53dafc8398e53b8240baf75b50539baea04a
Charlie Doughertye <cedougherty@wisc.edu>
2025-03-10 15:11:17 -0500

    Effort

--- scripts/TVLakes_Sediment/TVLakes_comparison_of_ENV_Variables.R
+++ scripts/TVLakes_Sediment/TVLakes_comparison_of_ENV_Variables.R
@@ -6,5 +6,36 @@
 library(tidyverse)
 library(lubridate)
 
+# met data
+setwd("~charliedougherty")
+BOYM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_boym_15min-20250205.csv") |> 
+  mutate(date_time = ymd_hms(date_time), 
+         year = year(date_time),
+         month = month(date_time),
+         week = week(date_time))
+
+HOEM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_hoem_15min-20250205.csv") |> 
+  mutate(date_time = ymd_hms(date_time), 
+         year = year(date_time),
+         month = month(date_time),
+         week = week(date_time))
+
+FRLM <- read_csv("~/Google Drive/My Drive/MCMLTER_Met/met stations/mcmlter-clim_frlm_15min-20250205.csv") |> 
+  mutate(date_time = ymd_hms(date_time), 
+         year = year(date_time),
+         month = month(date_time),
+         week = week(date_time))
+
 # add in data
-ice_thickness <- read_csv("data/lake ice/mcmlter")
+setwd("~/Documents/R-Repositories/MCM-LTER-MS")
+
+ice_thickness <- read_csv("data/lake ice/mcmlter-lake-ice_thickness-20250218_0.csv") |> 
+  mutate(date_time = mdy_hm(date_time), 
+         year = year(date_time), 
+         month = month(date_time), 
+         week = week(date_time))
+
+# goal here is to compare weather variables with ice thicknesses. Could do a 
+# weekly mean
+ice_thickness_weekly <- ice_thickness |> 
+  
