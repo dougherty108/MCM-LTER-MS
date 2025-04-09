1e0eb022c00d4fcb246f3414cde6a48a15569a4a
Charlie Doughertye <cedougherty@wisc.edu>
2025-04-09 09:55:34 -0500

    Trying to figure out some plotting issues. Ggplot wants to put the north at the bottom of the page, not the top, but then the coords get messed up when you try to flip it. Getting a little frustrated.

--- scripts/TVLakes_Sediment/TVLakes_plotting_separate_lakes.R
+++ scripts/TVLakes_Sediment/TVLakes_plotting_separate_lakes.R
@@ -5,6 +5,7 @@ library(lubridate)
 library(stars)
 library(MetBrewer)
 library(RColorBrewer)
+library(ggspatial)
 
 setwd("~/Google Drive/My Drive/EarthEngine/landsat/20250325")
 files <- list.files(pattern = ".tif")
@@ -61,7 +62,6 @@ for (i in 1:length(files)) {
 
 
 ######## USE THE BELOW IF YOU HAVE TO PLOT ONLY A SINGLE LAKE OUT OF THE DIRECTORY #########
-
 setwd("~/Google Drive/My Drive/EarthEngine/landsat/20250325")
 
 # Filter files to only include those with "HOA"
@@ -76,7 +76,7 @@ get_type <- function(filename) {
 }
 
 # Create output directory for HOA
-output_base <- "~/Google Drive/My Drive/EarthEngine/plots/20250328"
+output_base <- "~/Google Drive/My Drive/EarthEngine/plots/20250409"
 hoa_output <- file.path(output_base, "HOA")
 dir.create(hoa_output, showWarnings = FALSE, recursive = TRUE)
 
@@ -89,7 +89,9 @@ for (i in seq_along(files)) {
     drop_na()
   
   raster_df <- raster_df |> 
-    mutate(sediment_abundance = (1 - ice_endmember))
+    mutate(sediment_coverage = (1 - ice_endmember)#, 
+          # y = y*-1
+           )
   
   year <- str_extract(files[[i]], "20\\d{2}-\\d{2}-\\d{2}")
   type <- get_type(files[[i]])
@@ -98,11 +100,18 @@ for (i in seq_along(files)) {
     plot_path <- file.path(hoa_output, paste0("LANDSAT_plot_HOA_", year, ".png"))
     
     ggplot() +
-      geom_raster(data = raster_df, aes(x = x, y = y, fill = sediment_abundance)) +
-      coord_sf() +
+      geom_raster(data = raster_df, aes(x = x, y = y, fill = sediment_coverage)) +
+      coord_sf(crs = 3031,
+               datum = sf::st_crs("EPSG:3031")
+               ) +
+      #scale_y_reverse() + 
+      #scale_x_reverse() + 
       scale_fill_gradientn(colors = met_palette) +
       labs(title = paste0("HOA - ", year), x = "Easting", y = "Northing") +
-      theme_minimal()
+      annotation_north_arrow(location = "tr", which_north = "true",
+                             style = north_arrow_fancy_orienteering) +
+      annotation_scale(location = "bl", width_hint = 0.3) + 
+      theme_linedraw()
     
     setwd(hoa_output)
     ggsave(filename = plot_path)
@@ -112,5 +121,23 @@ for (i in seq_along(files)) {
 
 
 
+raster_df_flipped <- raster_df %>%
+  mutate(y_plot = -y)  # This flips it vertically for plotting only
+
+ggplot() +
+  geom_raster(data = raster_df_flipped, aes(x = x, y = y_plot, fill = sediment_coverage)) +
+  #scale_y_continuous(
+  #  name = "Northing",
+  ##  breaks = scales::pretty_breaks(n = 5),
+  #  labels = function(b) format(-b, scientific = FALSE)  # Restore original y-labels
+  #) +
+  scale_x_continuous(name = "Easting") +
+  scale_fill_gradientn(colors = met_palette) +
+  #labs(title = paste0("HOA - ", year)) +
+  coord_sf(crs = 3031) +
+  annotation_north_arrow(location = "tr", which_north = "true",
+                         style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  theme_linedraw()
 
 
