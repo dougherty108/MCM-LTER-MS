1cf2a5834b0639a3b35963c42575cb300b73798b
Charlie Doughertye <cedougherty@wisc.edu>
2025-05-05 14:54:26 -0500

    Comparing the pan band to the lake ice data. there doesn't really seem to be a relationship

--- scripts/TVLakes_Sediment/panchromatic_sma_comparison.R
+++ scripts/TVLakes_Sediment/panchromatic_sma_comparison.R
@@ -4,24 +4,55 @@ library(tidyverse)
 library(lubridate)
 
 # load data
-sma = read_csv("data/sediment abundance data/LANDSAT_sediment_abundances_20250403.csv")
+sma = read_csv("data/sediment abundance data/LANDSAT_sediment_abundances_20250403.csv") |> 
+  mutate(type = "sma") |> 
+  select(date, lake, sediment_abundance, type)
 
 pan = read_csv("data/sediment abundance data/LANDSAT_panchromatic.csv") |> 
-  mutate(sediment_corrected = 1-sediment_corrected)
+  mutate(sediment_abundance = 1-sediment_corrected) |> 
+  mutate(type = "panchromatic") |> 
+  select(date, lake, sediment_abundance, type)
 
-
-total_data = sma |> 
-  left_join(pan, by = join_by(date, lake))
-
-
-# plot data against each other
-ggplot(total_data, aes(sediment_abundance, sediment_corrected)) + 
-  geom_point() + 
-  facet_wrap(vars(lake)) + 
-  theme_linedraw(base_size = 15)
+total_data = rbind(sma, pan) |> 
+  mutate(year = year(date))
 
 # shows that the pan band really doesn't work. estaimtes up towards 0.75 sed at times (may not be the correc way to interpret)
 
 # load ice thickness data 
-lakeice = read_csv('data/lake ice/mcmlter-lake-ice_thickness-20250218_0_2025.csv')
+lakeice = read_csv('data/lake ice/mcmlter-lake-ice_thickness-20250218_0_2025.csv') |> 
+  mutate(date_time = mdy_hm(date_time), 
+         year = year(date_time), 
+         month = month(date_time)) |> 
+  filter(date_time >= "2016-05-01") |> 
+  #mutate(season = sapply(date_time, get_season)) |> 
+  filter(str_detect(string = location, pattern = "incubation hole") | 
+           str_detect(string = location, pattern = "sample hole")) |> 
+  filter(grepl("^O", location)) |> 
+  drop_na(z_water_m)
+
+li_summary = lakeice |> 
+  mutate(week = week(date)) |> 
+  group_by(year, month, location_name) |> 
+  summarize(mean_thickness = -1*mean(z_water_m, na.rm = T))
+
+## sediment
+sed_monthly <- total_data |> 
+  mutate(week = week(date), 
+         month = month(date)) |> 
+  group_by(year, month, lake, type) |>
+  summarize(mean_sed = mean(sediment_abundance, na.rm = T)) |> 
+  filter(type == "panchromatic")
+
+fulljoined = full_join(sed_monthly, li_summary) |> 
+  drop_na() |> 
+  mutate(month = as.character(month))
+
+##### plot
+ggplot(fulljoined, aes(mean_sed, mean_thickness)) + 
+  geom_smooth(method = "lm", se = T) + 
+  geom_point(aes(fill = month), size = 3, shape = 21) + 
+  facet_wrap(vars(lake), scales = "free") + 
+  ggtitle("October-February") + 
+  xlab("Mean Sediment Abundance (%)") + ylab("Mean Ice Thickness (m)") + 
+  theme_linedraw(base_size = 28) 
 
